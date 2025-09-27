615288c960e9406a75e0fb8637b44bb3ebad0677
MsKeen <>
2025-09-27 22:15:04 +0200

    First Update

--- main.cpp
+++ main.cpp
@@ -5,11 +5,29 @@ int main() {
     //--------------------------INITIALIZE---------------------------------------------------------
     sf::ContextSettings settings;
     settings.antialiasingLevel=8;
-    sf::RenderWindow window(sf::VideoMode(800,600),"RPG Game",sf::Style::Default,settings);
+    sf::RenderWindow window(sf::VideoMode(1920,1080),"RPG Game",sf::Style::Default,settings);
 
     //--------------------------INITIALIZE---------------------------------------------------------
 
     //--------------------------LOAD---------------------------------------------------------
+
+    //--------------------------SKELETON---------------------------------------------------------
+    sf::Texture enemyTexture;
+    sf::Sprite enemySprite;
+    if(enemyTexture.loadFromFile("C:/Users/mertc/CLionProjects/RPG-Game/Assets/Skeleton/Textures/SpriteSheet.png")){
+        std::cout<<"Player Texture Loaded"<<std::endl;
+        enemySprite.setTexture(enemyTexture);
+        enemySprite.setPosition(sf::Vector2f(400,100));
+
+        //X, Y, Width, Height
+        int XIndex=0;
+        int YIndex=2;
+        enemySprite.setTextureRect(sf::IntRect(XIndex*64,YIndex*64,64,64));
+        enemySprite.setScale(sf::Vector2f(3,3));
+
+    } else {std::cout<<"Player Texture Error"<<std::endl;}
+    //--------------------------SKELETON---------------------------------------------------------
+    //--------------------------PLAYER---------------------------------------------------------
 
     sf::Texture playerTexture;
     sf::Sprite playerSprite;
@@ -19,12 +37,14 @@ int main() {
         playerSprite.setTexture(playerTexture);
 
         //X, Y, Width, Height
-        int XIndex=8;
-        int YIndex=3;
+        int XIndex=0;
+        int YIndex=00;
         playerSprite.setTextureRect(sf::IntRect(XIndex*64,YIndex*64,64,64));
         playerSprite.setScale(sf::Vector2f(3,3));
     }
     else {std::cout<<"Player Texture Error"<<std::endl;}
+    //--------------------------PLAYER---------------------------------------------------------
+
 
     //--------------------------LOAD---------------------------------------------------------
 
@@ -33,7 +53,7 @@ int main() {
         //------------------------UPDATE---------------------------------------------------------
         sf::Event event;
         while(window.pollEvent(event)) {
-            if(event.type==sf::Event::Closed) {
+            if(event.type==sf::Event::Closed)
                 window.close();
             }
 
@@ -45,26 +65,28 @@ int main() {
             // } //not good
             sf::Vector2f position=playerSprite.getPosition();
             if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-                    playerSprite.setPosition(position+ sf::Vector2f(10 ,0));
+                    playerSprite.setPosition(position+ sf::Vector2f(1 ,0));
             }
             if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-                playerSprite.setPosition(position - sf::Vector2f(10,0));
+                playerSprite.setPosition(position - sf::Vector2f(1,0));
             }
             if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-                playerSprite.setPosition(position + sf::Vector2f(0,10));
+                playerSprite.setPosition(position + sf::Vector2f(0,1));
             }
             if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-                playerSprite.setPosition(position - sf::Vector2f(0,10));//or (position + sf::Vector2f(0,-10))
+                playerSprite.setPosition(position - sf::Vector2f(0,1));//or (position + sf::Vector2f(0,-10))
             }
+
 
             //------------------------UPDATE---------------------------------------------------------
 
             //------------------------DRAW---------------------------------------------------------
             window.clear(sf::Color::Black);
             window.draw(playerSprite);
+            window.draw(enemySprite);
             window.display();
             //------------------------DRAW---------------------------------------------------------
-        }
+
     }
 
 }
